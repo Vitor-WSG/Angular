aa56857ba34113b28531c8857f0fae864e79d79c
Vitor Gomes <60221263+Vitor-WSG@users.noreply.github.com>
2022-04-19 21:59:46 -0300

    Read sistema de cadastro Angular

--- crud/frontend/src/app/app.module.ts
+++ crud/frontend/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component'
 
+import { MatSnackBarModule } from '@angular/material/snack-bar'
 import { MatButtonModule } from '@angular/material/button'
 import { MatCardModule } from '@angular/material/card'
 import { MatSidenavModule } from '@angular/material/sidenav'
@@ -16,6 +17,12 @@ import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import {ProductCreateComponent} from './components/product/product-create/product-create.component'
+import { HttpClientModule } from '@angular/common/http';
+
+import { FormsModule } from '@angular/forms'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component'
 
 
 
@@ -27,7 +34,8 @@ import {ProductCreateComponent} from './components/product/product-create/produc
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
-    ProductCreateComponent
+    ProductCreateComponent,
+    ProductReadComponent
   ],
   imports: [
     BrowserModule,
@@ -37,7 +45,12 @@ import {ProductCreateComponent} from './components/product/product-create/produc
     MatSidenavModule,
     MatListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- crud/frontend/src/app/components/product/product-create/product-create.component.ts
+++ crud/frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService} from './../product.service'
+import { ProductService } from './../product.service'
+import { Router } from '@angular/router'
+import { Product } from './../produtc.model'
 
 @Component({
   selector: 'app-product-create',
@@ -8,9 +10,26 @@ import { ProductService} from './../product.service'
 })
 export class ProductCreateComponent implements OnInit {
 
-  constructor(private productService: ProductService) { }
+  product: Product ={
+    name: '',
+    price: 0
+  }
+
+  constructor(private productService: ProductService,
+    private router: Router) { }
 
   ngOnInit(): void {
+   
+  }
+  createProduct(): void{
+    this.productService.create(this.product).subscribe(()=> {
+      this.productService.showMessage('Produto criado')
+      this.router.navigate(['/products'])
+    })
+    
+  }
+  cancel(): void{
+    this.router.navigate(['/products'])
   }
 
 }
