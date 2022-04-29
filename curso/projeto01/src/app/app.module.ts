15c576d171981a734ee0f9e03f44294cd332275e
Vitor Gomes <60221263+Vitor-WSG@users.noreply.github.com>
2022-04-29 19:35:59 -0300

    Curso Angular
    Conteúdo aula 28 ngClass

--- curso/projeto01/src/app/app.module.ts
+++ curso/projeto01/src/app/app.module.ts
@@ -9,7 +9,10 @@ import { AppComponent } from './app.component';
 import { Project2Component } from './components/project2/project2.component';
 import {CursoModule} from './components/curso/curso.module';
 import { DataBindingComponent } from './components/data-binding/data-binding.component';
-import { ContadorComponent } from './components/contador/contador.component'
+import { ContadorComponent } from './components/contador/contador.component';
+import { NgIfComponent } from './components/ng-if/ng-if.component';
+import { NgSwitchComponent } from './components/ng-switch/ng-switch.component';
+import { NgClassComponent } from './components/ng-class/ng-class.component'
 
 
 @NgModule({
@@ -17,7 +20,10 @@ import { ContadorComponent } from './components/contador/contador.component'
     AppComponent,
     Project2Component,
     DataBindingComponent,
-    ContadorComponent
+    ContadorComponent,
+    NgIfComponent,
+    NgSwitchComponent,
+    NgClassComponent
   ],
   imports: [
     BrowserModule,
